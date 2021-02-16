df8d5a228bfca1dc791c9df51c24e37e7b0251a7
Hibee1 <49033033+Hibee1@users.noreply.github.com>
2021-02-16 13:39:11 +0100

    New changes

--- tic-tac-toe/src/Components/Board.js
+++ tic-tac-toe/src/Components/Board.js
@@ -1,24 +1,93 @@
 import React, {Component} from 'react'
 import Squares from './squares'
 
+const calculateWinner = (squares) => {
+    const lines = [
+        //horizontal
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+
+        //vertical
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        //diagonal
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+        //get current line row
+        const [a, b, c] = lines[i]
+
+        //keeps check if we have X or O in position a
+        if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+            return squares[a]
+        }
+        //i = 0
+        //lines[0] = 0, 1, 2
+        // a = 0, b = 1, c = 2
+        //if an element inside squares[0] exisis
+        // and if it's equal to squares[1]
+        // and if it's equal to squares[2]
+        // return that element (X or O)
+
+        //i = 1
+        //lines[1] = 3, 4, 5
+        // a = 3, b = 4, c = 5
+        //if an element inside squares[3] exisis
+        // and if it's equal to squares[4]
+        // and if it's equal to squares[5]
+        // return that element (X or O)
+    }
+    // if there's no such element - return an empty string
+    return ''
+}
+
 class Board extends Component {
 
     state = ({
-        squares: ['', '', '', '', '', '', '', '', '']
+        squares: ['', '', '', '', '', '', '', '', ''],
+        isNext: true
     })
 
+    handleReset = () => {
+        this.setState({
+            squares: ['', '', '', '', '', '', '', '', ''],
+            isNext: true
+        })
+    }
+
+
     handleClick = number => () => {
+        if (calculateWinner(this.state.squares) || this.state.squares[number]) {
+            // there's already an X or O here
+            return
+        }
+
         const newSquares = [...this.state.squares]
-        newSquares[number] = 'X'
+        newSquares[number] = this.state.isNext ? 'X' : 'O'
         this.setState ({
-            squares: newSquares
+            squares: newSquares,
+            isNext: !this.state.isNext
         })
     }
 
     render () {
-        const {squares} = this.state
+        const {squares, isNext} = this.state
+        const winner = calculateWinner(this.state.squares)
+        let status
+
+        if (winner) {
+            status = `Winner: ${winner}`
+        }
+        else {
+            status = `Next player: ${isNext ? 'X' : 'O'}`
+        }
         return (
             <div className="Game">
+                <h1 className="Declarer">{status}</h1>
                 <div className= "parent">
                     <div className="row">
                         <Squares onClick= {this.handleClick(0)} value= {squares[0]} />
@@ -36,6 +105,7 @@ class Board extends Component {
                         <Squares onClick= {this.handleClick(8)} value= {squares[8]} />
                     </div>
                 </div>
+                <button className= "Button" onClick= {this.handleReset}>Reset</button>
             </div>
         )
     }
